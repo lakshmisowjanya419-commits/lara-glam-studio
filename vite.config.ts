cb39d6481fd31d512fc538e0e1a9c208cea85325
lakshmisowjanya419-commits <lakshmisowjanya419@gmail.com>
2025-12-22 01:44:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/lara-glam-studio/",   // 👈 ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
@@ -16,3 +17,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
